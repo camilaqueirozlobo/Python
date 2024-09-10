46eb1b8254e5b34592da550130e19ad9049aec2e
Camila Queiroz <camilaqmlobo@gmail.com>
2024-09-10 10:09:14 -0300

    Add files via upload

---
+++ 04-nomeeidade.py
@@ -0,0 +1,11 @@
+#atribuição de variáveis
+nome= "Camila"
+idade= 16
+altura=1.60
+
+#exibir mensagem na tela 
+print("nome:",nome, "idade:", idade, "altura",altura)
+# formatação com f -string
+print(f"nome: {nome} idade:{idade} altura: {altura}")
+
+nome= input("digite o seu nome:")
---
+++ aula03.py
@@ -0,0 +1,12 @@
+teste = "Mila"
+print(teste)
+print(type(teste))
+teste = 10 
+print (teste)
+print (type(teste))
+teste = 1.60
+print(teste)
+print(type(teste))
+teste = True
+print(teste)
+print(type(teste))
