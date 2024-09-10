bad758ff67dc8ee8f8dc2529f9108c1074af4090
Camila Queiroz <camilaqmlobo@gmail.com>
2024-09-10 10:42:42 -0300

    Add files via upload

---
+++ 05-tipos-dados.py
@@ -0,0 +1,15 @@
+ # Tipos de dados em Pyton 
+#Inteiros 
+inteiro = 10
+print(inteiro)
+# Floats
+float = 10.5
+print(float)
+# strings 
+strings = "olá mundo!"
+print(string)
+# Booleanos 
+boleano = true 
+print(boleano)
+
+
