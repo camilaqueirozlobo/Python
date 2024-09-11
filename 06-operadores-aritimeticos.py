bd85dc371d8ec5fc2608b08c16b5280b22ccc3a4
Camila Queiroz <camilaqmlobo@gmail.com>
2024-09-11 10:51:05 -0300

    Add files via upload

---
+++ 06-operadores-aritimeticos.py
@@ -0,0 +1,22 @@
+# Operadores aritméticos
+# Soma
+soma = 10 + 5
+print(soma)
+# Subtração
+Subtracao = 10 - 5
+print(Subtracao)
+# Multiplicação
+multiplicacao = 10 * 5
+print(multiplicacao)
+# Divisão
+divisao = 10/5
+print(divisao)
+# Divisão inteira
+divisao_inteiro = 10 // 5
+print(divisao_inteiro)
+# Exponenciação
+exponenciacao = 10 ** 5
+print (exponenciacao)
+# Módulo / Resto da Divisão
+modulo = 10 % 5
+print(modulo)
